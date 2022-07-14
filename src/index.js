27575446c34971fe2456ae8b1a7b6dcfab414e9d
manishfw13 <manish_fw13_347@masai.school>
2022-07-14 13:03:23 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var first_name = "Karan";
+var age = 19;
+var isfetching = false;
+isfetching = true;
+var array = [1, 2, 3, 4, 5];
+var str = ["masai", "school", "full stack web developer"];
+var tuple;
+tuple = ["masai", true];
+var person;
+(function (person) {
+    person["USER"] = "Manish kumar";
+    person["SUPERUSER"] = "Kamlesh prasad singh";
+    person["ADMIN"] = "Babita";
+    person["SUPERADMIN"] = "Manorma Devi";
+})(person || (person = {}));
+function mult(x, y) {
+    return x * y;
+}
+mult(2, 3);
+function divided(x, y) {
+    return x / y;
+}
+divided(12, 3);
+function user(a) {
+    return a;
+}
+user("masai");
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+ let first_name : string = "Karan";
+
+ let age : number = 19;
+
+ let isfetching : boolean = false;
+
+ isfetching = true;
+
+ let array : number[] = [1, 2, 3, 4, 5]
+
+ let str : string[] = ["masai", "school", "full stack web developer"]
+
+ let tuple : [string, boolean]
+ tuple = ["masai", true]
+
+ enum person {
+    USER = "Manish kumar",
+    SUPERUSER = "Kamlesh prasad singh",
+    ADMIN = "Babita",
+    SUPERADMIN = "Manorma Devi"
+ }
+
+ function mult(x:number, y:number):number{
+    return x * y
+ }
+ mult(2,3)
+
+ function divided(x:number, y:number):number{
+    return x / y
+ }
+ divided(12,3)
+
+ function user(a:string){
+    return a
+ }
+
+ user("masai")
